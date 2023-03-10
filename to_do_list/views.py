d91ef35d28406e5d587f26088228044ba710cd5d
mrTchou <dev.mrtchou@gmail.com>
2023-03-11 00:31:57 +0100

    amelioration css. Ajout des tags. Deadline

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -6,9 +6,12 @@ def todolist(request):
     if request.method == 'POST':
         tache = request.POST.get('tache')
         deadline = request.POST.get('deadline')
+        priority = request.POST.get('priority')
         if tache:
-            list.append((tache, deadline))
+            list.append((tache, deadline, priority))
             print("La tache a bien été ajoutée")
         else:
             list.append(print("Veuillez saisir une tache"))
     return render(request, 'todolist.html', {'list': list})
+
+
