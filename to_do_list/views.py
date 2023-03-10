898440972a8e5ee72f51a11a00ac1a2efc8d3592
mrTchou <dev.mrtchou@gmail.com>
2023-03-10 12:22:59 +0100

    ajout de possibilité d'ajouter une date limite à une tache - modification de la boucle for dans le fichier html, et recuperation via POST la date que l'utilisateur a rentrer

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -5,9 +5,10 @@ list = []
 def todolist(request):
     if request.method == 'POST':
         tache = request.POST.get('tache')
+        deadline = request.POST.get('deadline')
         if tache:
-            list.append(tache)
+            list.append((tache, deadline))
             print("La tache a bien été ajoutée")
         else:
-            print("Veuillez saisir une tache")
+            list.append(print("Veuillez saisir une tache"))
     return render(request, 'todolist.html', {'list': list})
