073df7f6ab088fb15f5906120fb565e77fa5e76b
mrTchou <dev.mrtchou@gmail.com>
2023-03-12 23:59:33 +0100

    ajout fonctionalite update-a transformer et optimiser dans le futur

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -8,4 +8,5 @@ from . import views
 urlpatterns = [
     path('', views.todolist, name='todolist'),
     path('delete', views.delete_item_to_do_list, name='delete_item_to_do_list'),
+    path('update_item_to_do_list/', views.update_item_to_do_list, name='update_item_to_do_list'),
 ]
--- to_do_list/views.py
+++ to_do_list/views.py
@@ -19,12 +19,21 @@ def todolist(request):
 def delete_item_to_do_list(request):
     if request.method == 'POST':
         task_index = int(request.POST.get('task_index'))
-        print(task_index)
-        if task_index == 'update':
-            list.insert(task_index)
-            print(task_index)
-        else:
-            list.pop(task_index)
-            print(task_index)
+        list.pop(task_index)
     return redirect('todolist')
 
+
+
+
+def update_item_to_do_list(request):
+    if request.method == 'POST':
+        task_index = int(request.POST.get('task_index'))
+        task_name = request.POST.get('task')
+        deadline = request.POST.get('deadline')
+        priority = request.POST.get('priority')
+        if task_name:
+            list[task_index] = (task_name, deadline, priority)
+            print("La tache a bien été mise à jour")
+        else:
+            print("Veuillez saisir une tache")
+    return redirect('todolist')
