ef80709f5369f4e46378256281b7b23cea541cb9
mrTchou <dev.mrtchou@gmail.com>
2023-03-16 23:32:10 +0100

    ajout page update pour modifier dans une page a part la tache.recupere all objects.recupere objects avec id passé via url

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('todolist', views.todolist, name='todolist'),
     path('delete', views.delete_item_to_do_list, name='delete_item_to_do_list'),
     path('update_item_to_do_list/', views.update_item_to_do_list, name='update_item_to_do_list'),
+    path('update/<int:pk>/', views.update, name="update"),
 ]
--- to_do_list/views.py
+++ to_do_list/views.py
@@ -20,6 +20,18 @@ def index(request):
 	return render(request, "index.html", {"tasks": tasks,"task_form": form})
 
 
+def update(request, pk):
+	task = Task.objects.get(id=pk)
+	form = TaskForm(instance=task)
+	if request.method == "POST":
+		form = TaskForm(request.POST, instance=task)
+		if form.is_valid():
+			form.save()
+			return redirect("index")
+	return render(request, "update.html", {"edit_task_form": form})
+
+
+    
 
 
 
