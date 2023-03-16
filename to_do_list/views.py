592ca42814f64bde607ef21d0f646e2c0d236d8f
mrTchou <dev.mrtchou@gmail.com>
2023-03-16 16:18:28 +0100

    cration index.html, puis formulaire saisi taches simple avec un btn, creatiion fichier form avec class modelForm-tout fonctionne!

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-
-
-
+# Create your views here.
 def index(request):
-    return render(request, "index.html")
+	form = TaskForm()
+    #vérifier la méthode HTTP
+	if request.method == "POST":
+        #instancier le formulaire avec les données 
+		form = TaskForm(request.POST)
+        #tester la validité du formulaire
+		if form.is_valid():
+            #enregister les données
+			form.save()
+            #rediriger vers l'URL "index"
+			return redirect("index")
+
+	tasks = Task.objects.all()
+	return render(request, "index.html", {"tasks": tasks,"task_form": form})
+
 
 
 
