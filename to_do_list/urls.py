2d5b663dcc4eb4560329da662697f3fa8feeb37e
mrTchou <dev.mrtchou@gmail.com>
2023-03-17 16:04:26 +0100

    ajout link fichier urls, pour delete qui aura aussi pk en int

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('delete', views.delete_item_to_do_list, name='delete_item_to_do_list'),
     path('update_item_to_do_list/', views.update_item_to_do_list, name='update_item_to_do_list'),
     path('update/<int:pk>/', views.update, name="update"),
+    path('delete/<int:pk>/', views.delete, name="delete"),
 ]
